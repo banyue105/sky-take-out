28b01a469e453cceb44581f0e99340d44b86446d
banyue105 <banyue3074382140@163.com>
2026-08-12 15:11:14 +0800

    实现提交订单功能

--- sky-server/src/main/java/com/sky/interceptor/JwtTokenUserInterceptor.java
+++ sky-server/src/main/java/com/sky/interceptor/JwtTokenUserInterceptor.java
@@ -47,7 +47,7 @@ public class JwtTokenUserInterceptor implements HandlerInterceptor {
             log.info("jwt校验:{}", token);
             Claims claims = JwtUtil.parseJWT(jwtProperties.getUserSecretKey(), token);
             Long userId = Long.valueOf(claims.get(JwtClaimsConstant.USER_ID).toString());
-            log.info("当前员工id：", userId);
+            log.info("当前员工id:{}", userId);
             //设置当前登录员工id
             BaseContext.setCurrentId(userId);
             //3、通过，放行
