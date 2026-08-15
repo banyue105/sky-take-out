02c71678bf42326d1ff319f6e4da06021a7033e5
banyue105 <banyue3074382140@163.com>
2026-08-15 20:33:57 +0800

    实现数据统计excel表格导出

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 
 @RestController
@@ -88,4 +89,14 @@ public class ReportController {
         return Result.success(salesTop10ReportVO);
     }
 
+    /**
+     * 导出数据
+     * @param httpResponse
+     */
+    @GetMapping("/export")
+    @ApiOperation("导出数据")
+    public void export(HttpServletResponse httpResponse) {
+        log.info("导出数据");
+        reportService.export(httpResponse);
+    }
 }
